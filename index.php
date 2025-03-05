b31ae1dcdd5513ddc7c32210755a904ec510b950
Priya Kumari <kumaripriya3909@gmail.com>
2025-03-05 19:39:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
+   <link rel="stylesheet" href="css/style.css">
+  <title>My Website</title>
+</head>
+
+<body>
+  <!-- Header -->
+
+   
+
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero">
+            <h1><span>T</span>ax <span>R</span>egime</h1>
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home" style="color: #03f3d3;">Home</a></li>
+            <!-- <li><a href="tax\index\record.html" data-after="Service">Record</a></li> -->
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Hello, <span></span></h1>
+        <h1>Login & Register<span></span></h1>
+        <h1>Hear <span></span></h1>
+        <a href="login.php" type="button" class="cta">Login</a>
+        <a href="register.php" type="button" class="cta">Sign Up</a>
+      </div>
+    </div>
+  </section>
+
+   
+  <!-- End Hero Section  -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about">
+      <div class="col-left">
+        <div class="about-img">
+        <img src="image/tax.jpeg" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">About <span>us</span></h1>
+        <h2>TAX REGIME</h2>
+        <p> The New Tax Regime is a scheme of Income tax in India first proposed in Union Budget 2020–21. Subsequent Budget of FY2021-22 did not see any major announcements in this regime. During the Budget 2022–23, reports emerged that New Tax Regime was getting poor response and Government is considering to make it more attractive among the taxpayers.
+
+
+          Central Board of Direct Taxes (CBDT), Department of Revenue, Ministry of Finance, Government of India.
+          </p>
+        <a href="https://en.m.wikipedia.org/wiki/New_Tax_Regime " class="cta">Learn more</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Phone</h1>
+            <h2>+1 234 123 1234</h2>
+            <h2>+1 234 123 1234</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>info@gmail.com</h2>
+            <h2>abcd@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>Fatikchhari, Chittagong, Bangladesh</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>T</span>ax <span>R</span>egime</h1>
+      </div>
+      <h2>Your Complete Web Solution</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/linkedin.png"></a>
+        </div>
+      </div>
+      <p>Copyright © 2024 TAX REGIME. All rights reserved</p>
+    </div>
+  </section>
+  </div>
+  <!-- End Footer -->
+  <script src="src/app.js"></script>
+</body>
+
+</html>
